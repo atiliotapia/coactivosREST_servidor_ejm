9bf9c1f1cdee7c439b8d39a42c30bcf769693da2
atiliotapia <atilio.tapia@hotmail.com>
2012-10-09 21:35:26 -0500

    Update src/main/java/com/mycompany/rest/domain/PersonList.java
    Nueva Actualización en la clase PersonList.java

--- src/main/java/com/mycompany/rest/domain/PersonList.java
+++ src/main/java/com/mycompany/rest/domain/PersonList.java
@@ -3,7 +3,7 @@ package com.mycompany.rest.domain;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlRootElement;
-
+//Clase Lista Persona
 @XmlRootElement(name="persons")
 public class PersonList {
 
